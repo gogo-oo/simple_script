8b18e5f535ae4b3c03513aeb08899299f6091316
gogo-oo <gogo.oo.ua@gmail.com>
2017-09-08 14:16:26 +0300

    add "entry point" by src/main/java/oo/gogo/Main.java

---
+++ src/main/java/oo/gogo/Main.java
@@ -0,0 +1,10 @@
+package oo.gogo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("args=" + java.util.Arrays.toString(args));
+        System.out.println("Hello from entry point of \"Simple Script\", from file: src/main/java/oo/gogo/Main.java");
+    }
+
+}
